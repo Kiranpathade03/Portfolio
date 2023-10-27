dcd69436ddd1b171e34bb8e09205d0c53215784a
kiran pathade <117458423+Kiranpathade03@users.noreply.github.com>
2023-10-27 14:01:25 +0530

    Delete script1.js

--- script1.js
+++
@@ -1,8 +0,0 @@
-// Hire Me Javascript
-document.getElementById('emailButton').addEventListener('click', function() {
-    var email = "kiran1772002@gmail.com";
-    var subject = "Your Subject Here";
-    var body = "Your email body text here";
-    var mailtoLink = "mailto:" + email + "?subject=" + subject + "&body=" + body;
-    window.location.href = mailtoLink;
-});
